264e4e385c9ca628b71e571f2211a4a9a125139f
jainvjohn <johnvjain@gmail.com>
2024-11-27 22:49:42 +0530

    second commit

---
+++ src/main/java/com/ecom/pages/LoginPage.java
@@ -0,0 +1,6 @@
+package com.ecom.pages;
+
+public class LoginPage {
+
+
+}
---
+++ src/test/java/com/ecom/tests/LoginPageTest.java
@@ -0,0 +1,33 @@
+package com.ecom.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+public class LoginPageTest {
+    WebDriver driver;
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+         driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+       driver.get("https://www.flipkart.com/");
+    }
+    @Test
+    public void getTitleTest()
+    {
+        String loginPageTitle=driver.getTitle();
+        assert loginPageTitle != null;
+        boolean flag=loginPageTitle.contains("Online Shopping");
+        Assert.assertTrue(flag);
+    }
+}
