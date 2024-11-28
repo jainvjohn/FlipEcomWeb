8d33605f835569ab27134cdadef203025d8e4182
jainvjohn <johnvjain@gmail.com>
2024-11-29 01:28:01 +0530

    added 2 test cases

--- src/test/java/com/ecom/tests/LoginPageTest.java
+++ src/test/java/com/ecom/tests/LoginPageTest.java
@@ -1,14 +1,17 @@
 package com.ecom.tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
+import java.util.List;
 
 public class LoginPageTest {
     WebDriver driver;
@@ -31,11 +34,26 @@ public class LoginPageTest {
         Assert.assertTrue(flag);
     }
 
-//    @Test
-//    public void getURL()
-//    {
-//        String loginPageTitle=driver.getTitle();
+    @Test
+    public void getSearchProduct() throws InterruptedException {
+        driver.findElement(By.xpath("//input[@title='Search for Products, Brands and More']")).sendKeys("iphones");
+
+        List<WebElement> searchList=driver.findElements(By.xpath("//input[@title='Search for Products, Brands and More']/../..//following-sibling::ul/li//a"));
+for(WebElement searchListItem: searchList)
+{
+    System.out.println(searchListItem.getText());
+
+}
+        System.out.println("Total no of suggestions list is "+searchList.size());
+        //            String loginPageTitle=driver.getTitle();
 //        assert loginPageTitle != null;
 //        boolean flag=loginPageTitle.contains("Online Shopping");
 //        Assert.assertTrue(flag);
-}
+    }
+        @AfterMethod
+                public void tearDown()
+        {
+        driver.close();
+        }
+    }
+
